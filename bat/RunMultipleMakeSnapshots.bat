e73e1674bbf7c44645506055ae1335b2a15190ef
Jip Claassens <jclaassens@objectvision.nl>
2023-03-22 12:50:38 +0100

    edit batch

--- bat/RunMultipleMakeSnapshots.bat
+++ bat/RunMultipleMakeSnapshots.bat
@@ -1,180 +1,116 @@
-set geodmsversion=GeoDMS7314
+set geodmsversion=GeoDMS8.8.0
 set exe_dir=C:\Program Files\ObjectVision\%geodmsversion%
 set ProgramPath=%exe_dir%\GeoDMSRun.exe
-set LOCALDATA_PROJDIR=C:\GeoDMS\LocalData\BAG\MakeSnapshot\CalcCachex64.v7.314
 
 cd ..\cfg
 
-echo %DATE% %TIME%
-
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
-set snapshot_date_env=20120101
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+REM set snapshot_date_env=20120101
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20120401
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20120701
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20121001
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
-set snapshot_date_env=20130101
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+REM set snapshot_date_env=20130101
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20130401
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20130701
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20131001
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
-set snapshot_date_env=20140101
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+REM set snapshot_date_env=20140101
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20140401
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20140701
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20141001
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
-set snapshot_date_env=20150101
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+REM set snapshot_date_env=20150101
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20150401
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20150701
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20151001
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
-set snapshot_date_env=20160101
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+REM set snapshot_date_env=20160101
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20160401
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20160701
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20161001
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
-set snapshot_date_env=20170101
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+REM set snapshot_date_env=20170101
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20170401
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20170701
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20171001
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
-set snapshot_date_env=20180101
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+REM set snapshot_date_env=20180101
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20180401
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20180701
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20181001
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
-set snapshot_date_env=20190101
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+REM set snapshot_date_env=20190101
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20190401
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20190701
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20191001
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
-set snapshot_date_env=20200101
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+REM set snapshot_date_env=20200101
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20200401
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20200701
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20201001
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
-set snapshot_date_env=20210101
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+REM set snapshot_date_env=20210101
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20210401
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 set snapshot_date_env=20210701
-"%ProgramPath%" BAG20_MakeSnaphot.dms /snapshot_date_nl_vreemdeeruit/MaakVolledigeSnapshot 
-del "%LOCALDATA_PROJDIR%" /S /Q
-echo %DATE% %TIME%
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
+set snapshot_date_env=20211001
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 
+REM set snapshot_date_env=20220101
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
+set snapshot_date_env=20220401
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
+set snapshot_date_env=20220701
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
+set snapshot_date_env=20221001
+"%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
+
+REM set snapshot_date_env=20230101
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
+REM set snapshot_date_env=20230401
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
+REM set snapshot_date_env=20230701
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
+REM set snapshot_date_env=20231001
+REM "%ProgramPath%" BAG20_MakeSnaphot.dms /Snapshots/Ruimtescanner/MaakVolledigeSnapshot_metWPopVoorraadNiveau
 
 
 
